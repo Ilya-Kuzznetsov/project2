142848404f60606ab04ef506d16b1e44b19384fc
Илюша <ilya-kuzznetsov@yandex.ru>
2019-01-26 18:35:53 +0300

    2 страницы в мобилке

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var prefixer = require("gulp-autoprefixer");
 gulp.task("less", function () {
-  return gulp.src("less/style.less")
+  return gulp.src("less/main.less")
 .pipe(less())
 .pipe(gulp.dest("css"));
 });
